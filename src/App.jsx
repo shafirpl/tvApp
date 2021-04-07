d49c3be5e6ec23eb9e9674d24f2920846a35352f
shafirpl <shafi.rpl@gmail.com>
2021-04-07 06:59:00 -0700

    code modified

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowAltLeft, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+let limit = 1000;
+let totalSpend = 0;
 toast.configure()
 function App() {
 const [cartButtonClicked, setCartButtonClicked] = useState(false)
 //const [cartButtonClicked, setCartButtonClicked] = useState(true)
 const [vidPlaying, setVidPlaying] = useState(true)
 const [showSideBar, setShowSideBar] = useState(false)
+const [itemName,setItemName] = useState("");
 const vidRef = useRef(null)
-const purchase = () => {toast.success("Item Purchased and on your way!", {
-  position: toast.POSITION.TOP_CENTER,
-  autoClose: 1000,
-  hideProgressBar: true,
-});}
+//let limit = 1000;
+// const [limit, setLimit] = useState(1000)
+const [itemPrice, setItemPrice] = useState(0);
+const purchase = () => {
+if(limit-itemPrice>=0){
+  limit -= itemPrice
+  totalSpend += itemPrice
+  console.log(`Total spending is ${totalSpend}, limit remains ${limit}`)
+  toast.success("Item Purchased and on your way!", {
+    position: toast.POSITION.TOP_CENTER,
+    autoClose: 2000,
+    hideProgressBar: true,
+    })
+  toast.warn(`You have ${limit}$ remaining`, {
+    position: "bottom-center",
+    autoClose: 2000,
+    hideProgressBar: true,
+  });
+}
+else{
+  toast.error("Sorry, you don't have enough credit", {
+    position: toast.POSITION.TOP_CENTER,
+    autoClose: 2000,
+    hideProgressBar: true,
+    })
+}
+;}
 
 const push = () => {toast("Pushed, please check your phone!", {
-  position: toast.POSITION.TOP_CENTER,
-  autoClose: 1000,
-  hideProgressBar: true,
+position: toast.POSITION.TOP_CENTER,
+autoClose: 1000,
+hideProgressBar: true,
 });}
 
 
@@ -51,7 +75,7 @@ return (
         <FontAwesomeIcon icon={faShoppingCart} /></Button>
     </div>
     <div className="Video">
-    <ToastContainer />
+      <ToastContainer />
       <ResponsiveEmbed aspectRatio="16by9">
         <video ref={vidRef} controls autoPlay muted>
           <source src="./video.mp4" type="video/mp4" />
@@ -61,34 +85,58 @@ return (
       {cartButtonClicked?(
       <div className="recommendation">
         <div className="flex-scroll">
-          <div className="box" onClick={ ()=> {setShowSideBar(true)}}>
-            <ResponsiveEmbed aspectRatio="16by9">
-              <embed type="image/jpg" src="./watch-2.jpg" />
+          <div className="box" onClick={ ()=> {
+            setItemPrice(20)
+            setItemName("Pink Kenzo Dress")
+            setShowSideBar(true)
+            }}>
+            <ResponsiveEmbed aspectRatio="4by3">
+              <embed type="image/jpg" src="./pinkdress.jpg" />
             </ResponsiveEmbed>
           </div>
-          <div className="box">
-            <ResponsiveEmbed aspectRatio="16by9">
-              <embed type="image/jpg" src="./watch-1.jpg" />
+          <div className="box" onClick={ ()=> {
+            setItemPrice(600)
+            setItemName("iPhone 11")
+            setShowSideBar(true)
+            }}>
+            <ResponsiveEmbed aspectRatio="4by3">
+              <embed type="image/jpg" src="./iphone.jpg" />
             </ResponsiveEmbed>
           </div>
-          <div className="box">
-            <ResponsiveEmbed aspectRatio="16by9">
-              <embed type="image/png" src="./bowtie.png" />
+          <div className="box" onClick={ ()=> {
+            setItemPrice(160)
+            setItemName("Airpods")
+            setShowSideBar(true)
+            }}>
+            <ResponsiveEmbed aspectRatio="4by3">
+              <embed type="image/jpg" src="./airpods.jpg" />
             </ResponsiveEmbed>
           </div>
-          <div className="box">
-            <ResponsiveEmbed aspectRatio="16by9">
-              <embed type="image/png" src="./necklace.png" />
+          <div className="box" onClick={ ()=> {
+            setItemPrice(600)
+            setItemName("iPad Pro")
+            setShowSideBar(true)
+            }}>
+            <ResponsiveEmbed aspectRatio="4by3">
+              <embed type="image/jpg" src="./ipad.jpg" />
             </ResponsiveEmbed>
           </div>
-          <div className="box">
-            <ResponsiveEmbed aspectRatio="16by9">
-              <embed type="image/png" src="./earning.png" />
+          <div className="box" onClick={ ()=> {
+            setItemPrice(20)
+            setItemName("Neon Lights")
+            setShowSideBar(true)
+            }}>
+            <ResponsiveEmbed aspectRatio="4by3">
+              <embed type="image/jpeg" src="./lights.jpeg" />
             </ResponsiveEmbed>
           </div>
-          <div className="box">
-            <ResponsiveEmbed aspectRatio="16by9">
-              <embed type="image/png" src="./earning.png" />
+          <div className="box" onClick={ ()=> {
+            setItemPrice(20)
+            setItemName("Camera Case")
+            setShowSideBar(true)
+            }}>
+            <ResponsiveEmbed aspectRatio="4by3">
+              <embed type="image/jpg" src="./camera.jpg" />
             </ResponsiveEmbed>
           </div>
         </div>
@@ -101,15 +149,15 @@ return (
     <div className="SideMenu">
       <ul className="SideMenuList">
         <li>
-          <h1>Omega 50 Watch</h1>
+          <h1> {`${itemName}`} </h1>
         </li>
         <li onClick={purchase}>
           <h1 className="listHeader">Purchase</h1>
         </li>
-        <li onClick = {push}>
+        <li onClick={push}>
           <h1 className="listHeader">Push This To Phone</h1>
         </li>
-        <li onClick = {push}>
+        <li onClick={push}>
           <h1 className="listHeader">Push All To Phone</h1>
         </li>
       </ul>
